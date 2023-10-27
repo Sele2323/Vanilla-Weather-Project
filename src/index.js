bf96d7a649de2732fa60245098e765d21c1ec59b
Sele2323 <selemengmatsoso@gmail.com>
2023-10-27 12:52:33 +0200

    updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    let hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    let minutes = `0${hours}`;
+  }
+  let days = [
+    "sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = (document.querySelector("#temperature").innerHTML =
@@ -11,6 +35,9 @@ function displayTemperature(response) {
   let windElement = (document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   ));
+  timeElement = document.querySelector("#time").innerHTML = formatDate(
+    response.data.dt + 1000
+  );
 }
 
 let apiKey = "93deded4294438a23eae320afe21c38d";
